ebe5661620f06c95056a6120f1b8957d74254b19
bryan201887 <bryan.villavicencios@ug.edu.ec>
2020-07-26 00:38:43 -0500

    version1

---
+++ src/curso/util/util.java
@@ -0,0 +1,17 @@
+
+package curso.util;
+
+
+
+/**
+ *
+ * @author Antum
+ */
+
+public class util {
+    
+    public static String url = "jdbc:postgresql://localhost:5432/cursoonline";
+    public static String user = "postgres";
+    public static String password = "1234";
+    
+}
